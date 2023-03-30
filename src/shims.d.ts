7a53b6dff4744105d4f4fb196bd413e17490fdb1
sanbo <sanbo.xyz@gmail.com>
2023-03-30 14:31:49 +0800

    update sth. at 2023-03-30 14:31:49

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -7,18 +7,18 @@ import type { AttributifyAttributes } from "@unocss/preset-attributify"
 // }
 
 declare global {
-  namespace astroHTML.JSX {
-    type HTMLAttributes = AttributifyAttributes
-  }
-  namespace JSX {
-    type HTMLAttributes<T> = AttributifyAttributes
-  }
+    namespace astroHTML.JSX {
+        type HTMLAttributes = AttributifyAttributes
+    }
+    namespace JSX {
+        type HTMLAttributes<T> = AttributifyAttributes
+    }
 }
 
 declare module "solid-js" {
-  namespace JSX {
-    interface Directives {
-      clickOutside: any
+    namespace JSX {
+        interface Directives {
+            clickOutside: any
+        }
     }
-  }
 }
--- src/system.ts
+++ src/system.ts
@@ -1,13 +1,13 @@
 import type { Model } from "./types"
 
 export const defaultSetting = {
-  continuousDialogue: true,
-  archiveSession: false,
-  openaiAPIKey: "",
-  openaiAPITemperature: 60,
-  password: "",
-  systemRule: "",
-  model: "gpt-3.5-turbo" as Model
+    continuousDialogue: true,
+    archiveSession: false,
+    openaiAPIKey: "",
+    openaiAPITemperature: 60,
+    password: "",
+    systemRule: "",
+    model: "gpt-3.5-turbo" as Model
 }
 
 export const defaultMessage = ``
@@ -17,9 +17,9 @@ export type Setting = typeof defaultSetting
 export const defaultResetContinuousDialogue = false
 
 export const defaultMaxInputTokens: Record<Model, number> = {
-  "gpt-3.5-turbo": 3072,
-  "gpt-4": 6144,
-  "gpt-4-32k": 24576
+    "gpt-3.5-turbo": 3072,
+    "gpt-4": 6144,
+    "gpt-4-32k": 24576
 }
 
 export const defaultModel: Model = "gpt-3.5-turbo"
